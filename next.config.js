2b4c4316db1820ee3400d1b77369b9a2f607f3b5
Ben Fortune <ben.fortune@gmail.com>
2022-08-17 20:58:04 -0500

    adds todo comment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // TODO uncomment the following when firebase is updated to v9
   // experimental: {
   //   runtime: 'experimental-edge'
   // }
